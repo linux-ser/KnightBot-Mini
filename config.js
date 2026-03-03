0ce74fef88042020b5b1d1c5bf7cecfb25464d56
𝐋ιɴυх Sєʀ 👽📈 <callmebro2244@gmail.com>
2026-03-03 16:50:25 +0530

    Update bot owner and configuration details

--- config.js
+++ config.js
@@ -4,11 +4,11 @@
 
 module.exports = {
     // Bot Owner Configuration
-    ownerNumber: ['91xxxxxxxxxxx','917023951514'], // Add your number without + or spaces (e.g., 919876543210)
-    ownerName: ['Knight Bot Mini', 'Professor'], // Owner names corresponding to ownerNumber array
+    ownerNumber: ['91xxxxxxxxxxx','919562530542'], // Add your number without + or spaces (e.g., 919876543210)
+    ownerName: ['linux ser', 'Editor'], // Owner names corresponding to ownerNumber array
     
     // Bot Configuration
-    botName: 'Knight Bot Mini',
+    botName: '𝐋ɪɴᴜх 𝐒ᴇʀ 👽📈',
     prefix: '.',
     sessionName: 'session',
     sessionID: process.env.SESSION_ID || '',
@@ -16,7 +16,7 @@ module.exports = {
     updateZipUrl: 'https://github.com/mruniquehacker/KnightBot-Mini/archive/refs/heads/main.zip', // URL to latest code zip for .update command
     
     // Sticker Configuration
-    packname: 'Knight Bot Mini',
+    packname: 'í lσvє чσu pσnnє 🥹🫶🏻  𝐋ɪɴᴜх 𝐒ᴇʀ 👽📈',
     
     // Bot Behavior
     selfMode: false, // Private mode - only owner can use commands
@@ -81,9 +81,8 @@ module.exports = {
     
     // Social Links (optional)
     social: {
-      github: 'https://github.com/mruniquehacker',
-      instagram: 'https://instagram.com/yourusername',
-      youtube: 'http://youtube.com/@mr_unique_hacker'
+      github: 'https://github.com/linux-ser',
+      instagram: 'https://instagram.com/linux_ser',
     }
 };
   
